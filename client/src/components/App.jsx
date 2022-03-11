270f83f60e832f69da5fd514b69eaad71f041a51
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-03-11 09:14:38 -0500

    Just Starting

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import movies from '../data/exampleMovieData.js';
+
+@param {movies} object
 
 const App = (props) => (
-  <div>Hello World!</div>
+
+
+  <div>
+    <h2>Movie List</h2>
+    {movies.map(movies => (
+      <p>{movies.title}</p>
+    ))}
+  </div>
+
 );
 
 export default App;
